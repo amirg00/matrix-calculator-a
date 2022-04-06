a0599588ea710e183617faf45c9cfc3a9cd1542d
AmirGill <amirg00@walla.com>
2022-04-06 14:55:05 +0300

    ADD more tests to Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -9,11 +9,13 @@
 #include "Matrix.hpp"
 using namespace zich;
 
-vector<double> getRandVecMat(unsigned long size);
+vector<double> getRandVecMat(int size);
 Matrix getRandMatrix();
+double floatRand(double min, double max);
 
 
-
+// Check cases in which matrices dimension isn't the same.
+// In such cases, an exception should be thrown by definition.
 TEST_CASE("Matrices dimension isn't the same"){
     srand(time(0)); /* create random generator*/
     Matrix randMat = getRandMatrix();
@@ -39,8 +41,9 @@ TEST_CASE("Comparison Operators"){
 
 
 TEST_CASE("Increment - Decrement Matrix"){
-    unsigned long row = 1 + rand() % 100;
-    unsigned long col = 1 + rand() % 100;
+    /* Create random matrix */
+    int row = 1 + rand() % 100;
+    int col = 1 + rand() % 100;
     cout << row << ", " << col << endl;
     vector<double> randVecMat = getRandVecMat(row * col);
     Matrix randMat{randVecMat, row, col};
@@ -48,32 +51,36 @@ TEST_CASE("Increment - Decrement Matrix"){
     vector<double> randVecMatCopy(randVecMat);
     Matrix expectedRandMat{randVecMatCopy, row, col};
 
+    /* Check postfix '--' operation */
     CHECK((randMat-- == expectedRandMat));
     for (double &element : randVecMatCopy) {element--;}
     Matrix expectedRandMat2{randVecMatCopy, row, col};
     CHECK((randMat == expectedRandMat2));
 
+    /* Check postfix '++' operation */
     CHECK((randMat++ == expectedRandMat2));
     for (double &element : randVecMatCopy) {element++;}
     Matrix expectedRandMat3{randVecMatCopy, row, col};
     CHECK((randMat == expectedRandMat3));
 
+    /* Check prefix '--' operation */
     for (double &element : randVecMatCopy) {--element;}
     Matrix expectedRandMat4{randVecMatCopy, row, col};
     CHECK((--randMat == expectedRandMat4));
 
+    /* Check prefix '++' operation */
     for (double &element : randVecMatCopy) {++element;}
     Matrix expectedRandMat5{randVecMatCopy, row, col};
     CHECK((++randMat == expectedRandMat5));
 }
 
 TEST_CASE("Unary Operations"){
-    unsigned long row = 1 + rand() % 100;
-    unsigned long col = 1 + rand() % 100;
+    /* Create random matrix */
+    int row = 1 + rand() % 100;
+    int col = 1 + rand() % 100;
     cout << row << ", " << col << endl;
     vector<double> randVecMat = getRandVecMat(row * col);
     Matrix randMat{randVecMat, row, col};
-
     CHECK((+randMat == randMat));
     Matrix randMatUnary = +randMat;
     CHECK_FALSE((&randMatUnary == &randMat)); /* Check that it returns a copy*/
@@ -87,9 +94,10 @@ TEST_CASE("Unary Operations"){
 }
 
 TEST_CASE("Scalar Multiplication"){
-    unsigned long row = 1 + rand() % 100;
-    unsigned long col = 1 + rand() % 100;
-    int scalar = rand() % 100;
+    /* Create random matrix and scalar*/
+    int row = 1 + rand() % 100;
+    int col = 1 + rand() % 100;
+    double scalar = round(floatRand(-100, 100) * 100.0) / 100.0;
     cout << row << ", " << col << endl;
     vector<double> randVecMat = getRandVecMat(row * col);
     vector<double> randVecMatCopy(randVecMat);
@@ -108,11 +116,56 @@ TEST_CASE("Scalar Multiplication"){
     Matrix MultiplicationLeft = scalar * randMat;
     CHECK((MultiplicationLeft == randMatCopy));
 
-    /* Check *= method */
+    /* Check *= method for scalar */
     CHECK_NOTHROW(randMat *= scalar);
     CHECK((randMat == randMatCopy));
 }
 
+TEST_CASE("Matrix Multiplication"){
+    std::vector<double> arr1 = {1, 1, 0, 1, 0, 0, 1, 1};
+    std::vector<double> arr2 = {3, 0, 3, 0, 3, 3, 0, 3};
+    std::vector<double> arr3 = {6, 3, 3, 6};
+    Matrix a{arr1, 2, 4};  // constructor taking a vector and a matrix size
+    Matrix b{arr2, 4, 2};  // constructor taking a vector and a matrix size
+    Matrix result{arr3, 2, 2};
+    CHECK((a*b == result));
+
+    std::vector<double> arr4 = {1, 0, 0, 1, 0, 0, 1, 0, 0};
+    std::vector<double> arr5 = {3, 0, 0, 3, 0, 0, 3, 0, 0};
+    Matrix c{arr4, 3, 3};  // constructor taking a vector and a matrix size
+    Matrix d{arr5, 3, 3};  // constructor taking a vector and a matrix size
+    Matrix result2{arr5, 3, 3};
+    CHECK((c*d == result2));
+
+    /* Invalid case by matrix multiplication def.: e's cols isn't equal to f's row*/
+    Matrix e{arr1, 4, 2};
+    Matrix f{arr2, 4, 2};
+    CHECK_THROWS(e*f);
+}
+
+TEST_CASE("Constructor"){
+    /* Create random vector */
+    vector<double> randVecMat = getRandVecMat(10);
+
+    /* rows or cols are zero */
+    CHECK_THROWS((Matrix{{}, 0, 0}));
+
+    /* rows or cols are negative */
+    int randNegNum =  (-1) * (rand() % 100);
+    CHECK_THROWS((Matrix{randVecMat, randNegNum, 10}));
+    CHECK_THROWS((Matrix{randVecMat, 10, randNegNum}));
+    CHECK_THROWS((Matrix{randVecMat, -2, -5})); /* row * col = 10 and vector size is 10, but they are negative*/
+
+
+    /* rows & cols are positive but don't match vector size*/
+    CHECK_THROWS((Matrix{randVecMat, 3, 3}));
+
+}
+
+TEST_CASE("Print Random Matrix"){
+    Matrix mat = getRandMatrix();
+    CHECK_NOTHROW(cout << mat << '\n');
+}
 
 
 
@@ -124,9 +177,9 @@ double floatRand(double min, double max)
     return min + randFloat * (max - min);
 }
 
-vector<double> getRandVecMat(unsigned long size){
+vector<double> getRandVecMat(int size){
     vector<double> randVecMat;
-    for (unsigned long i = 0; i < size; i++){
+    for (int i = 0; i < size; i++){
         double randNum = round(floatRand(-100, 100) * 100.0) / 100.0;
         randVecMat.push_back(randNum);
     }
@@ -134,8 +187,8 @@ vector<double> getRandVecMat(unsigned long size){
 }
 
 Matrix getRandMatrix(){
-    unsigned long row = 1 + rand() % 100;
-    unsigned long col = 1 + rand() % 100;
+    int row = 1 + rand() % 100;
+    int col = 1 + rand() % 100;
     cout << row << ", " << col << endl;
     vector<double> randVecMat = getRandVecMat(row * col);
     cout << randVecMat.size() << endl;
